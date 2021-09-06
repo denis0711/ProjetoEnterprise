b4e13bad0d2c66b9fc1db3e67a7a84832e9174bf
Denis <denisapsilva07@gmail.com>
2021-09-06 13:54:24 -0300

    Adicionar arquivos de projeto.

---
+++ src/Services/NSE.Identidade.API/Models/UsuarioRespostaLogin.cs
@@ -0,0 +1,9 @@
+﻿namespace NSE.Identidade.API.Models
+{
+    public class UsuarioRespostaLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UsuarioToken UsuarioToken { get; set; }
+    }
+}
