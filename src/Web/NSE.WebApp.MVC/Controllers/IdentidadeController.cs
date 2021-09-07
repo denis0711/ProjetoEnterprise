b975c8e2f9ebe90e7b6b537b8bef15bd74e3999a
Denis <denisapsilva07@gmail.com>
2021-09-07 20:26:24 -0300

    criando todas as models e implementadando a base da controller indentidade e criei as views

--- src/Web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/Web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,44 @@ namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
+
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return View(usuarioRegistro);
+
+         
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+           
+        }
+
+
     }
 }
---
+++ src/Web/NSE.WebApp.MVC/Models/UsuarioRespostaLogin.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.WebApp.MVC.Models
+{
+    public class UsuarioRespostaLogin
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UsuarioToken UsuarioToken { get; set; }
+        //public ResponseResult ResponseResult { get; set; }
+    }
+}
