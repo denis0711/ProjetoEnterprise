1ae8ad18552095a33f50c294f20c405cb4e883d0
Denis <denisapsilva07@gmail.com>
2021-09-09 18:10:41 -0300

    Obter dados do usuario, criando uma interface iUser e usando outras classe para auxilio

--- src/Web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/Web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Services;
+using Microsoft.AspNetCore.Http;
+using NSE.WebApp.MVC.Extensions;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -9,6 +11,10 @@ namespace NSE.WebApp.MVC.Configuration
         public static void RegisterServices(this IServiceCollection services)
         {
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped<IUser, AspNetUser>();
         }
     }
 }
