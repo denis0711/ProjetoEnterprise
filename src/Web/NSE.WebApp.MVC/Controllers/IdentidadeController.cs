2268dd4dd7a99d891c6f349f84f738bdb69d7d78
Denis <denisapsilva07@gmail.com>
2021-09-10 18:20:42 -0300

    tratamentos de erro 2

--- src/Web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/Web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -47,15 +47,18 @@ namespace NSE.WebApp.MVC.Controllers
 
         [HttpGet]
         [Route("login")]
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl = null)
         {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
 
         [HttpPost]
         [Route("login")]
-        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin, string returnUrl = null)
         {
+
+            ViewData["ReturnUrl"] = returnUrl;
             if (!ModelState.IsValid) return View(usuarioLogin);
              
             //API - Login
@@ -67,7 +70,9 @@ namespace NSE.WebApp.MVC.Controllers
 
             await RealizarLogin(resposta);
 
-            return RedirectToAction("Index", "Home");
+           if(string.IsNullOrEmpty(returnUrl)) return RedirectToAction("Index", "Home");
+
+            return LocalRedirect(returnUrl);
         }
 
         [HttpGet]
--- src/Web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
+++ src/Web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -32,7 +32,7 @@ namespace NSE.WebApp.MVC.Extensions
         {
             if (httpRequestException.StatusCode == HttpStatusCode.Unauthorized)
             {
-                context.Response.Redirect($"/login" /*?ReturnUrl={context.Request.Path}*/);
+                context.Response.Redirect($"/login?ReturnUrl={context.Request.Path}");
                 return;
             }
 
